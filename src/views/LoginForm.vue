949d34290acc474d6a1be2a222fd3520c3773f5c
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2023-06-20 12:05:10 +0100

    home styling

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="">
-    <h1>Rightcharge Coding Test</h1>
-    <p>Create a Coverage Overview tool</p>
-    <p>
-      Purpose: Create a dynamic “Coverage Overview” map that allows the user to
-      view the parts of the UK that are covered by the installers in our
-      network. The user should also be able to select any combination of
-      chargers, installers, or areas (postcode and range combinations) to narrow
-      down the results.
-    </p>
-    <p>
-      If you can't build the project or run into any issues where you cannot
-      continue please reach out to me by email before wasting hours and I'll
-      help you along.
-    </p>
+  <div class="flex flex-wrap justify-center items-center text-white">
+    <div class="flex flex-wrap justify-center items-center text-white">
+      <div class="text-xl mb-10">Create a Coverage Overview tool</div>
+
+      <div class="w-full">
+        Purpose: Create a dynamic “Coverage Overview” map that allows the user
+        to view the parts of the UK that are covered by the installers in our
+        network. The user should also be able to select any combination of
+        chargers, installers, or areas (postcode and range combinations) to
+        narrow down the results.
+      </div>
+    </div>
   </div>
 </template>
